da1b27673c07f82ad50ac1436f56615748f0ea46
Maikalanimai <kalonekamimoto@gmail.com>
2019-12-05 16:20:54 -0700

    filterobj and even odd complete

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import './index.css'
-import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+// import TopicBrowser from './components/TopicBrowser/TopicBrowser'
 import Sum from './components/Topics/Sum'
 import Palindrome from './components/Topics/Palindrome'
 import FilterString from './components/Topics/FilterString'
-// import FilterObject from './components/Topics/FilterObject'
+import FilterObject from './components/Topics/FilterObject'
 import EvenAndOdd from './components/Topics/EvenAndOdd'
 
 class App extends Component {
   render() {
     return(<div>
       <EvenAndOdd />
-      <TopicBrowser />
+      <FilterObject />
       <Sum />
       <Palindrome />
       <FilterString />
--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,65 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
-class FilterObject extends Component{
-    render (){
-        return(<p>FilterObject Component</p>)
-    }
+class FilterObject extends Component {
+  constructor() {
+    super();
+    this.state = {
+      unFilteredArray: [
+        {
+          name: "Ashe",
+          role: "Marksman",
+          region: "Freljord",
+          specialEvent: true
+        },
+        {
+          name: "Tryndamere",
+          role: "Bruiser",
+          region: "Freljord",
+          reworkNeeded: true
+        },
+        {
+          name: "Shen",
+          role: "Bruiser",
+          region: "Ionia",
+          reworkNeeded: false
+        }
+      ],
+      userInput: "",
+      filteredArray: ""
+    };
+  }
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
+
+  filterArr() {
+    console.log(this);
+    let filtered = this.state.unFilteredArray.filter(element => {
+      return element.hasOwnProperty(this.state.userInput);
+    });
+    this.setState({ filteredArray: filtered });
+  }
+
+  render() {
+    return (
+      <div className="puzzleBox filterObjectPB">
+        <h4>Filter Object</h4>
+        <span className="puzzleText">
+          {JSON.stringify(this.state.unFilteredArray)}
+        </span>
+        <input
+          className="inputLine"
+          onChange={e => this.handleChange(e.target.value)}
+        ></input>
+        <button className="confirmationButton" onClick={() => this.filterArr()}>
+          Filter
+        </button>
+        <span className="resultsBox filterObjectRB">
+          {JSON.stringify(this.state.filteredArray)}
+        </span>
+      </div>
+    );
+  }
 }
 
 export default FilterObject;
