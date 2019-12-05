23648ad51cb86e89dfd153735a346cf6dbb59130
Maikalanimai <kalonekamimoto@gmail.com>
2019-12-05 15:05:36 -0700

    Even odd complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
+import './index.css'
+import TopicBrowser from './components/TopicBrowser/TopicBrowser'
+import Sum from './components/Topics/Sum'
+import Palindrome from './components/Topics/Palindrome'
+import FilterString from './components/Topics/FilterString'
+// import FilterObject from './components/Topics/FilterObject'
+import EvenAndOdd from './components/Topics/EvenAndOdd'
 
 class App extends Component {
   render() {
-    
+    return(<div>
+      <EvenAndOdd />
+      <TopicBrowser />
+      <Sum />
+      <Palindrome />
+      <FilterString />
+      
+
+    </div>
+    )
   }
 }
 
